935cc0f3fc9301f1e3c49d88766cb3cea211cf27
ThomasNoel <anon31@protonmail.com>
2023-11-23 16:18:23 +0100

    #000003: Install Cors package.

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 // Routes imports
 const userRouter = require('./routes/userRoutes');
 const jobRouter = require('./routes/jobRoutes');
@@ -8,13 +9,16 @@ const app = express();
 
 // ✅ Register the bodyParser middleware here
 app.use(bodyParser.json());
-app.use(
-    bodyParser.urlencoded({
-        extended: true,
-    }),
+app.use(bodyParser.urlencoded(
+        {
+            extended: true,
+        }
+    ),
 );
 
-// 1: MIDDLEWARE
+// 1: GLOBAL MIDDLEWARE
+app.use(cors());
+
 app.use((req, res, next) => {
     console.log('Hello from the middleware 👋');
     next();
